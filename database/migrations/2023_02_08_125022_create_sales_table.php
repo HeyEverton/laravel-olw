4beca7ae40a2856f85e0e8a87f6ee36c6ca5cb9b
Everton <92490650+HeyEverton@users.noreply.github.com>
2023-04-12 09:48:13 -0300

    commit for save

---
+++ database/migrations/2023_02_08_125022_create_sales_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('source');
+            $table->char('status', 1);
+            $table->integer('total_amount');
+            $table->dateTime('sold_at');
+
+            $table->foreignId('client_id')->constrained();
+            $table->foreignId('seller_id')->constrained();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+};
